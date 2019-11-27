76c21d9def5b6c494c491ebf27664d7c7f32002d
phatdroid94 <phatdroid94@gmail.com>
2019-11-27 20:04:19 +0700

    inflat layout menu

--- app/src/main/java/com/example/roomdatabase04092019/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase04092019/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.roomdatabase04092019;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_menu_insert :
+                Toast.makeText(this, "Item menu sert", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
