0d0821c94cdef6036a411c62de993ef798a8e1f1
phatdroid94 <phatdroid94@gmail.com>
2019-11-27 21:20:52 +0700

    finish setup adapter

--- app/src/main/java/com/example/roomdatabase04092019/SinhvienAdapter.java
+++ app/src/main/java/com/example/roomdatabase04092019/SinhvienAdapter.java
@@ -1,13 +1,40 @@
 package com.example.roomdatabase04092019;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SinhvienAdapter {
+import java.util.ArrayList;
 
+public class SinhvienAdapter extends RecyclerView.Adapter<SinhvienAdapter.SinhvienHolder>{
+
+    ArrayList<Sinhvien> sinhviens;
+
+    @NonNull
+    @Override
+    public SinhvienHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater  = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_sinhvien, parent, false);
+        return new SinhvienHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SinhvienHolder holder, int position) {
+        Sinhvien sinhvien = sinhviens.get(position);
+        holder.txtName.setText("Tên : " +sinhvien.getName());
+        holder.txtAge.setText("Năm sinh : " +sinhvien.getAge());
+        holder.txtAddress.setText("Địa chỉ : " +sinhvien.getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return sinhviens != null ? sinhviens.size() : 0;
+    }
 
     class SinhvienHolder extends RecyclerView.ViewHolder{
         TextView txtName,txtAge,txtAddress;
